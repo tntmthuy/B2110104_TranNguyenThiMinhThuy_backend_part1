b0e6699cd2bdbe4bc42dc316845840a2592ae18f
Minh Thuy <trannguyenminhthuyag@gmail.com>
2024-02-22 09:44:02 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,26 @@
+exports.create = (req,res) => {
+	res.send({message: "create handler"});
+};
+
+export.findAll = (req, res) => {
+	res.send({message: "findAll handler"});
+};
+
+export.findOne = (req, res) => {
+	res.send({message: "findOne handler"});
+};
+
+export.update = (req, res) => {
+	res.send({message: "update handler"});
+};
+
+export.delete = (req, res) => {
+	res.send({message: "delete handler"});
+};
+
+export.findAllFavorite = (req, res) => {
+	res.send({message: "findAllFavorite handler"});
+};
+
+
+
